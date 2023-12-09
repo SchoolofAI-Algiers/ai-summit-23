a341b92e2ae64ea72d4bdf82783bb0de383e9e8b
Abdelbasset10 <basset.rezazi26@gmail.com>
2023-12-09 15:43:27 +0100

    add hero section with counter down

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
-import About from './components/about/About'
+import Hero from './components/hero/Hero'
+import CounterContainer from './components/hero/CounterContainer'
 
 const App = () => {
   return (
     <div className='' >
       <Navbar />
-      <About />
+      <Hero />
+      <CounterContainer countDownLimit={1702713600000} />
+
     </div>
   )
 }
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='w-full mt-10 mb-16' >
+        <div className='px-4 lg:px-10 flex flex-col md:flex-row md:items-center md:justify-between gap-4'>
+            <div className='flex-1 flex flex-col items-center md:items-start gap-2' >
+                <p className='font-kanit' >School of ai Algiers</p>
+                <h1 className='text-4xl sm:text-5xl text-center md:text-start lg:text-6xl xl:text-7xl font-bold font-kanit uppercase' >new edition</h1>
+                <h1 className='text-4xl sm:text-5xl text-center md:text-start lg:text-6xl xl:text-7xl uppercase text-ai-blue font-bold font-kanit'>AI SUMMIT <span className='text-orange-400' >2023</span> Is here</h1>
+                <button className='bg-orange-400 hover:bg-orange-500 px-8 py-1 text-white rounded-md border-none w-fit' >Register Now</button>
+            </div>
+            <div className='flex-1 flex items-center justify-center' >
+                <img src="/images/hero-photo.png" alt="hero" className='w-[15rem] sm:w-[25rem] md:w-[25rem] lg:w-[32rem]' />
+            </div>
+            
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero
