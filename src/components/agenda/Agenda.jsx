f858b8ce54f0b9b1e2b3a50c805389ebc2e8c8ba
unknown <li_mokrane@esi.dz>
2023-12-13 09:22:49 +0100

    fix agenda

--- src/components/agenda/Agenda.jsx
+++ src/components/agenda/Agenda.jsx
@@ -10,12 +10,12 @@ const Agenda = () => {
         <img src={bg} alt="" className=' py-8 h-[60vh] md:h-auto w-full -z-10'/>
         <img src={blobRight} className='absolute lg:h-auto h-28 right-0 top-0' alt="" />
         <img src={blobLeft} className='absolute lg:h-auto  h-28 left-0 bottom-0' alt="" />
-        <div className=' md:mt-0 lg:pt-8 absolute h-full flex flex-col items-center justify-center md:justify-normal space-y-8 top-0 left-0 w-full text-center'>
+        <div className=' md:mt-0 lg:pt-8 absolute h-full flex flex-col items-center justify-center md:justify-normal space-y-4 top-0 left-0 w-full text-center'>
             <h1 className='title md:mt-28 text-white font-kanit text-3xl lg:text-6xl xl:text-7xl font-bold'>Agenda</h1>
-            <div className='bg-white rounded-xl w-[75%] md:h-[55%] md:w-[55%] p-4 lg:p-12 flex flex-col items-center justify-between '>
+            <div className='bg-white rounded-xl w-[75%] md:h-[55%] md:w-[55%] p-2 lg:p-8 flex flex-col items-center justify-between '>
                 {agenda.map(e=>(
-                    <div className='flex md:text-2xl lg:text-3xl w-full justify-around font-source-sans font-[700] '>
-                        <div className=' text-ai-orange'>
+                    <div className='flex md:text-2xl lg:text-3xl w-full justify-start font-source-sans font-[700] '>
+                        <div className=' text-ai-orange mr-[14%] '>
                             {e.from} - {e.to}
                         </div>
                         <div className=' text-ai-blue'>{e.title}</div>
--- src/data/agenda.js
+++ src/data/agenda.js
@@ -1,33 +1,33 @@
 export const agenda=[
     {
-        from:'9:00',
-        to:'10:00',
+        from:'08:30',
+        to:'09:00',
+        title:'Check-in'
+    },
+    {
+        from:'09:00',
+        to:'09:30',
         title:'Opening Ceremony'
     },
     {
-        from:'9:00',
-        to:'10:00',
-        title:'Opening Ceremony'
+        from:'09:45',
+        to:'12:00',
+        title:'Talks Sessions 1'
     },
     {
-        from:'9:00',
-        to:'10:00',
-        title:'Opening Ceremony'
+        from:'13:00',
+        to:'14:00',
+        title:'Talks Sessions 2'
     },
     {
-        from:'9:00',
-        to:'10:00',
-        title:'Opening Ceremony'
+        from:'14:00',
+        to:'16:00',
+        title:'Poster Session'
     },
     {
-        from:'9:00',
-        to:'10:00',
-        title:'Opening Ceremony'
-    },
-    {
-        from:'9:00',
-        to:'10:00',
-        title:'Opening Ceremony'
+        from:'16:30',
+        to:'17:00',
+        title:'Closing Ceremony'
     },
 ]
 
